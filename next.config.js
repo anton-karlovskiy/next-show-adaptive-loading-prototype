96c303096e4498bb3a155f14b4d2289ec2ad4b7d
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-12-13 12:00:22 +0300

    testing on glitch with ESM build

--- next.config.js
+++ next.config.js
@@ -15,29 +15,12 @@
  */
 
 // ray test touch <
-// const withTM = require('next-transpile-modules');
+const withTM = require('next-transpile-modules');
 // ray test touch >
 const withCSS = require('@zeit/next-css');
 
 // ray test touch <
-// module.exports = withTM(withCSS({
-//   webpack: function (config) {
-//     config.module.rules.push({
-//       test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-//       use: {
-//         loader: 'url-loader',
-//         options: {
-//           limit: 100000,
-//           name: '[name].[ext]'
-//         }
-//       }
-//     })
-//     return config
-//   },
-//   distDir: './build',
-//   transpileModules: ['react-adaptive-hooks']
-// }));
-module.exports = withCSS({
+module.exports = withTM(withCSS({
   webpack: function (config) {
     config.module.rules.push({
       test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
@@ -53,5 +36,22 @@ module.exports = withCSS({
   },
   distDir: './build',
   transpileModules: ['react-adaptive-hooks']
-});
+}));
+// module.exports = withCSS({
+//   webpack: function (config) {
+//     config.module.rules.push({
+//       test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+//       use: {
+//         loader: 'url-loader',
+//         options: {
+//           limit: 100000,
+//           name: '[name].[ext]'
+//         }
+//       }
+//     })
+//     return config
+//   },
+//   distDir: './build',
+//   // transpileModules: ['react-adaptive-hooks']
+// });
 // ray test touch >
